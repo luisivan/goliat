79ad864aee9f6eaa201d1dc196420fefa53b0b80
Luis Iván <me@luisivan.net>
2014-02-19 15:09:46 +0100

    Temas

--- temas.js
+++ temas.js
@@ -1,10 +1,7 @@
 var temas = {
 	"2º de Bachillerato": {
 		"Matemáticas aplicadas a las CCSS": [
-			"Operaciones de matrices",
-			"Rango de una matriz",
-			"Hallar la matriz inversa",
-			"Aplicación de matrices"
+			"Matrices"
 		]
 	}
 }
