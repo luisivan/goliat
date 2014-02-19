df11ca1d246f3ec4b162ca0a8fbc497a82446c31
Luis Iván <me@luisivan.net>
2014-02-19 14:50:41 +0100

    Temas

--- lista.js
+++ lista.js
@@ -23,10 +23,8 @@ for (var i=0; i<lostemas.length; i++) {
 	  rowHandler: template,
 	  userCallback: function () {
 	  	$('a.thumbnail').each(function(i, el) {
-	  		var video_id = $(el).data('link').split('v=')[1].split('&')[0],
-	  			ampersandPosition = video_id.indexOf('&')
-			if(ampersandPosition != -1)
-			  video_id = video_id.substring(0, ampersandPosition)
+	  		console.log('najaua')
+	  		var video_id = $(el).data('link').split('v=')[1].split('&')[0]
 
 			$(el).attr('href', 'video.html#'+video_id)
 		  	$(el).find('img').attr('src', 'http://img.youtube.com/vi/'+video_id+'/mqdefault.jpg')
