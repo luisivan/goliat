f8bc4056aa549859adeb996516204bae4b4f864d
Luis Iván <me@luisivan.net>
2014-03-11 22:31:27 +0100

    Update

--- temas.js
+++ temas.js
@@ -2,6 +2,10 @@ var temas = {
 	"2º de Bachillerato": {
 		"Matemáticas aplicadas a las CCSS": [
 			"Matrices"
+		],
+		"Biología": [
+			"Bioelementos y biomoléculas inorganicas",
+			"Los Glúcidos"
 		]
 	}
 }
